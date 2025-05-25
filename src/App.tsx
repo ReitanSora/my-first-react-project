22e56b463911495998840bab4234818e25a711f2
Edison <edi.camp@hotmail.com>
2025-05-24 23:50:47 -0500

    Mario Anfibio

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,13 @@ function App() {
 
   return (
     <div>
-      <div>
-              <a href="https://react.dev" target="_blank">
+      <div>        
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-  
       </div>
       <h1>Saludos a mis panas Anfibios</h1>
       <div className="card">
